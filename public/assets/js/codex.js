10d44098eeaa313d75f679c8c71785b83ac16213
Shea Lewis <shea.lewis89@gmail.com>
2014-11-02 01:09:13 -0800

    Added dynamic callouts

--- public/assets/js/codex.js
+++ public/assets/js/codex.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
 	// Prettify the <pre> tags
 	$('pre').addClass('prettyprint theme-freshcut');
 
+	// Dynamic callouts
+	$("blockquote:contains(Attention)").addClass('bs-callout bs-callout-danger');
+	$("blockquote:contains(Danger)").addClass('bs-callout bs-callout-danger');
+
+	$("blockquote:contains(Warning)").addClass('bs-callout bs-callout-warning');
+	$("blockquote:contains(Notice)").addClass('bs-callout bs-callout-warning');
+
+	$("blockquote:contains(Info)").addClass('bs-callout bs-callout-info');
+	$("blockquote:contains(Note)").addClass('bs-callout bs-callout-info');
+
+	$("blockquote:contains(Hint)").addClass('bs-callout bs-callout-success');
+	$("blockquote:contains(Tip)").addClass('bs-callout bs-callout-success');
 });
