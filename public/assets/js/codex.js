805380ff60eec9ecdd6781c5631a62f096ef5397
Shea Lewis <shea.lewis89@gmail.com>
2014-11-02 01:03:52 -0800

    Grammar update

--- public/assets/js/codex.js
+++ public/assets/js/codex.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	// Bootstrap the tables
 	$('table').addClass('table table-striped table-bordered table-hover table-condensed');
 
-	// Prettyprint the <pre> tags
+	// Prettify the <pre> tags
 	$('pre').addClass('prettyprint theme-freshcut');
 
 });
