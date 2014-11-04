1d73f7414017bc22b2af9d694cb35280ce257f5e
Shea Lewis <shea.lewis89@gmail.com>
2014-11-03 19:46:02 -0800

    Added favicon. Oops.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -4,6 +4,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
 		<title>Codex - {{ $currentManual }} v{{ $currentVersion }}</title>
 
 		<!-- CSS -->
