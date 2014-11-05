c6d27acff4b3bff090ec9cee1acfc119359964d3
Shea Lewis <shea.lewis89@gmail.com>
2014-11-04 18:00:50 -0800

    Better anchor support

--- public/assets/js/codex.js
+++ public/assets/js/codex.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
 			var anchor = $(this).text().toLowerCase().trim();
 
 			var hyphenNeedle = [/ /g];
-			var emptyNeedle = ['[', ']', ':'];
+			var emptyNeedle = ['[', ']', '(', ')', ':'];
 
 			hyphenNeedle.forEach(function(word) {
 				anchor = anchor.replace(word, "-");
