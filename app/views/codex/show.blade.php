a73ab38a9ccd807fae0137a595a2a346712ae307
Shea Lewis <shea.lewis89@gmail.com>
2014-11-06 11:09:35 -0800

    Added footer

--- app/views/codex/show.blade.php
+++ app/views/codex/show.blade.php
@@ -11,22 +11,34 @@
 				</div>
 
 				<div class="col-md-9 documentation">
+					<p class="pull-right">
+						<small>{{ $lastUpdated }}</small>
+					</p>
+
 					{{ $content }}
 
 					<hr>
 
 					<p class="pull-right">
-						<small>{{ $lastUpdated }}</small>
+						<small>
+							Powered by <a href="http://codex.caffeinated.ninja">Codex</a> v1.0.0
+						</small>
 					</p>
 				</div>
 			@else
 				<div class="col-md-12 documentation">
+					<p class="pull-right">
+						<small>{{ $lastUpdated }}</small>
+					</p>
+
 					{{ $content }}
 
 					<hr>
 
 					<p class="pull-right">
-						<small>{{ $lastUpdated }}</small>
+						<small>
+							Powered by <a href="http://codex.caffeinated.ninja">Codex</a> v1.0.0
+						</small>
 					</p>
 				</div>
 			@endif
--- app/views/search/show.blade.php
+++ app/views/search/show.blade.php
@@ -27,6 +27,14 @@
 						</p>
 					@endif
 				</div>
+
+				<hr>
+
+				<p class="pull-right">
+					<small>
+						Powered by <a href="http://codex.caffeinated.ninja">Codex</a> v1.0.0
+					</small>
+				</p>
 			</div>
 		</div>
 	</div>
