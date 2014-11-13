521fb4fb297fc9aeb0a58d63b0eaa53644f08d96
Shea Lewis <shea.lewis89@gmail.com>
2014-11-13 00:37:30 -0800

    Update footer version

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -2,6 +2,6 @@
 
 <p class="pull-right">
 	<small>
-		Powered by <a href="http://codex.caffeinated.ninja">Codex</a> 1.1.0-dev
+		Powered by <a href="http://codex.caffeinated.ninja">Codex</a> 1.1.0
 	</small>
 </p>
